4bb879c911130052a740179de2a49c2866ffe8b6
alondra-exe <171g0576@rcarbonifera.tecnm.mx>
2021-12-04 23:17:35 -0600

    HTML base.

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,1 +1,15 @@
-// JavaScript source code
+function instrucciones() {
+    document.querySelector("#modalInstrucciones").style.display = "block";
+}
+
+function ocultarInstrucciones() {
+    document.querySelector("#modalInstrucciones").style.display = "none";
+}
+
+function ocultarSeleccion() {
+    document.querySelector("#modalSeleccion").style.display = "none";
+}
+
+function nuevaPartida() {
+    window.location.reload(true);
+}
